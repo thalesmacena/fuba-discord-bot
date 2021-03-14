2420d6acdc42efa61719e52f2e9c8663035f1b38
Thales Macena <thalesmacena@gmail.com>
2021-03-14 17:22:49 -0300

    add skipCommand

---
+++ src/app/commands/music/SkipCommand.ts
@@ -0,0 +1,28 @@
+import { ICommand, IExecuteProps } from '../../api';
+
+const SkipCommand: ICommand = {
+  name: 'skip',
+  description: 'Pula uma música na lista de reprodução',
+  args: 'Nenhum argumento',
+  example: '`skip`',
+
+  execute: async ({ message, guildQueue }: IExecuteProps): Promise<void> => {
+    if (!message.member.voice.channel) {
+      message.reply(
+        'Você precisa estar em um canal de voz para passar a música'
+      );
+      return;
+    }
+
+    if (!guildQueue) {
+      message.reply('Não existe uma fila de músicas');
+      return;
+    }
+
+    guildQueue.connection.dispatcher.end();
+
+    guildQueue.textChannel.send('⏭  **Música pulada**');
+  }
+};
+
+export default SkipCommand;
