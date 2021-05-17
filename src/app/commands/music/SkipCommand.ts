2577b34743035dc4c53b1cae1c41cb2b4c1a4384
Thales Macena <thalesmacena@gmail.com>
2021-05-17 04:49:09 -0300

    add skip to position

--- src/app/commands/music/SkipCommand.ts
+++ src/app/commands/music/SkipCommand.ts
@@ -1,4 +1,5 @@
 import { ICommand, IExecuteProps } from '@/app/interfaces';
+import { musicPlayer } from '@/app/utils/musicPlayer';
 
 const SkipCommand: ICommand = {
   name: 'skip',
@@ -6,7 +7,12 @@ const SkipCommand: ICommand = {
   args: 'Nenhum argumento',
   example: '`skip`',
 
-  execute: async ({ message, guildQueue }: IExecuteProps): Promise<void> => {
+  execute: async ({
+    message,
+    argsProps,
+    guildQueue,
+    queue
+  }: IExecuteProps): Promise<void> => {
     if (!message.member.voice.channel) {
       message.reply(
         'Você precisa estar em um canal de voz para passar a música'
@@ -19,15 +25,58 @@ const SkipCommand: ICommand = {
       return;
     }
 
+    const { textChannel } = guildQueue;
+
     const { dispatcher } = guildQueue.connection;
 
     if (dispatcher.paused) {
       dispatcher.resume();
     }
 
+    const indexString = /\d+/.exec(argsProps.args[0]);
+
+    if (indexString) {
+      const index = parseInt(indexString[0], 10) - 1;
+      const songList = guildQueue.songs;
+      const song = songList[index];
+
+      if (!song) {
+        textChannel.send(
+          'Você precisa entrar como um numero válido da fila de músicas, tente `$queue`'
+        );
+        return;
+      }
+
+      textChannel.send(
+        `⏭ Pulando para ${song.title} na posição ${
+          index + 1
+        } da fila de Reprodução`
+      );
+
+      const newSongList = songList.slice(index);
+      const removedSongList = songList.slice(0, index - 1);
+
+      let uptime = 0;
+
+      removedSongList.forEach((songInList) => {
+        guildQueue.totalTime -= songInList.length;
+        uptime += songInList.length;
+      });
+
+      newSongList.forEach((songInList) => {
+        songInList.upTime -= uptime;
+      });
+
+      guildQueue.songs = newSongList;
+
+      musicPlayer(guildQueue.songs[0], message.guild.id, queue);
+
+      return;
+    }
+
     dispatcher.end();
 
-    guildQueue.textChannel.send('⏭  **Música pulada**');
+    textChannel.send('⏭  **Música pulada**');
   }
 };
 
